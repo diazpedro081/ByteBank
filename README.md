270ddf7722f024e110ceba6b20da7d7c336b334f
diazpedrodev <081diazpedro@gmail.com>
2022-08-17 16:44:34 -0300

    Polimorfismo

---
+++ README.md
@@ -0,0 +1,22 @@
+## Estudos
+
+Conteudo de estudos Java OO.
+
+## Getting Started
+
+Java no VSCODE
+
+## Folder Structure
+
+A área de trabalho contém duas pastas por padrão, onde:
+
+- `src`: A pasta para manter os fontes.
+- `lib`: A pasta para manter as dependências.
+
+Enquanto isso, os arquivos de saída compilados serão gerados na pasta `bin` fpor padrão.
+
+> Para personalizar a estrutura das pastas va em `.vscode/settings.json` .
+
+## Dependency Management
+
+> Mais detalhes em [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
---
+++ src/Conta.java
@@ -0,0 +1,73 @@
+public class Conta {
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+    private static int total;
+
+    public Conta(int agencia, int numero) {
+        Conta.total++;
+        this.agencia = agencia;
+        this.numero = numero;
+    }
+
+    void deposita(double valor) {
+        this.saldo += valor;
+    }
+
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if (this.saca(valor)) {
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
+
+    public double getSaldo(){
+        return this.saldo;
+    }
+
+    public int getAgencia (){
+        return this.agencia;
+    } 
+
+    public void setAgencia(int agencia) {
+        if(agencia <= 0){
+            System.out.println("nao pode valor menos igual a 0");
+            return;
+        }
+        this.agencia = agencia;
+    }
+
+    public int getNumero(){
+        return this.numero;
+    }
+
+    public void setNumero(int numero){
+        if(numero <= 0 ){
+            System.out.println("Nao pode valor menor igual a 0");
+            return;
+        }
+        this.numero = numero;
+    }
+
+    public Cliente getTitular(){
+        return this.titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    public static int getTotal() {
+        return Conta.total;
+    }
+}
